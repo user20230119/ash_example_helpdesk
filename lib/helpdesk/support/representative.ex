68aa4c25eeda11f810cced06bf23361ef218fa93
user20230119 <123089799+user20230119@users.noreply.github.com>
2024-10-14 13:18:41 -0400

    - Add AshPostgres.DataLayer - Run `mix ash.setup` - Run `mix ash.codegen initial_migration`

--- lib/helpdesk/support/representative.ex
+++ lib/helpdesk/support/representative.ex
@@ -1,7 +1,16 @@
 defmodule Helpdesk.Support.Representative do
   use Ash.Resource,
     otp_app: :helpdesk,
-    domain: Helpdesk.Support
+    domain: Helpdesk.Support,
+    data_layer: AshPostgres.DataLayer
+
+  # The Postgres keyword is specific to the AshPostgres module.
+  postgres do
+    # Tells Postgres what to call the table
+    table "representatives"
+    # Tells Ash how to interface with the Postgres table
+    repo Helpdesk.Repo
+  end
 
   actions do
     defaults [:read, create: [:name]]
